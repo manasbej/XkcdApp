4ed113a6be36fd43d26a26fcae07fe7d12076f59
manasbej <manasbej@gmail.com>
2021-07-01 20:48:29 +0530

    Create EmailService.php

---
+++ EmailService.php
@@ -0,0 +1,16 @@
+// Start code here
+// Class name: EmailService
+
+<?php
+class EmailService {
+  // Properties
+  public $sender;
+  public $receipient;
+
+  // Methods
+  function send_email_with_attachment() {
+    
+  }
+  
+}
+?>
