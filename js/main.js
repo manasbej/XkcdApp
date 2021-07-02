9df292aeaa0158142f848e320b880e60c76baad8
manasbej <manasbej@gmail.com>
2021-07-02 16:03:12 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Code Sample  
+$(document).ready(function() {  
+    var url = 'http://www.recruiter.com/feed/career.xml'; //Data in XML format  
+    alert('Started');  
+    $.ajax({  
+        type: 'GET',  
+        url: "https://api.rss2json.com/v1/api.json?rss_url=" + url, //For converting default format to JSON format  
+        dataType: 'jsonp', //for making cross domain call  
+        success: function(data) {  
+            alert('Success');  
+            $("#rss-default").append(data.feed);  
+            console.log(data.feed.description);  
+        }  
+    });  
+});  
