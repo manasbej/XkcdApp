7dbf805ec1f4a7f181c4003410ec7b1e3f08b24c
manasbej <manasbej@gmail.com>
2021-07-15 21:46:12 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 $(document).ready(function() {  
     var url = 'https://xkcd.com/rss.xml'; // 'http://www.recruiter.com/feed/career.xml'; //Data in XML format  
     
+    var password="abcd123";
+    
     $.ajax({  
         type: 'GET',  
         url: "https://api.rss2json.com/v1/api.json?rss_url=" + url, //For converting default format to JSON format  
